808fc7d82cdd9fa7a80fdccfd51bcbe24b536d6b
Eliaz Bobadilla <eliaz.bobadilladev@gmail.com>
2021-06-20 18:11:21 -0500

    :art: Update

---
+++ 08_ranges/36_range_include.rb
@@ -0,0 +1,4 @@
+# frozen_string_literal: true
+
+puts (1..6).include? 3
+puts (1..6).include? 30
---
+++ 08_ranges/37_range_letters.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+puts ('a'..'z').include? 'b' # true
+puts ('a'..'z').include? 'c' # true
+puts ('a'..'z').include? '?' # false
