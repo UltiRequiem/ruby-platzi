a5cf2cb3870ae17a4ebdacfff543edb50075a052
Eliaz Bobadilla <eliaz.bobadilladev@gmail.com>
2021-06-23 08:16:54 -0500

    :art: Add Ruby2D

---
+++ 15_ruby2d/49_triangle.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+require 'ruby2d'
+
+set title: 'Hello Triangle'
+
+Triangle.new(
+  x1: 320, y1:  50,
+  x2: 540, y2: 430,
+  x3: 100, y3: 430,
+  color: %w[red green blue]
+)
+
+show
---
+++ 15_ruby2d/50_square.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+require 'ruby2d'
+
+set width: 300, height: 200
+
+s = Square.new
+
+s.color = 'red'
+
+show
